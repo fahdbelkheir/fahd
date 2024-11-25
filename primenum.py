56716946412e5b96aa088328bbb6b1bc125cae33
fahdbelkheir <fahed.belkheiri@gmail.com>
2024-11-25 21:03:15 +0100

    primenum01

--- primenum.py
+++ primenum.py
@@ -1,15 +1,15 @@
 import math
 
-# Function to check if a number is prime
+
 def is_prime(N):
     if N <= 1:
-        return False  # 0 and 1 are not prime numbers
+        return False  
     if N <= 3:
-        return True   # 2 and 3 are prime numbers
+        return True   
     if N % 2 == 0 or N % 3 == 0:
-        return False  # Eliminate multiples of 2 and 3
+        return False  
 
-    # Check divisors from 5 to √N
+    
     i = 5
     while i * i <= N:
         if N % i == 0 or N % (i + 2) == 0:
@@ -17,17 +17,15 @@ def is_prime(N):
         i += 6
     return True
 
-# Main function
+
 def main():
-    # Prompt the user to enter an integer
+    
     number = int(input("Enter an integer: "))
 
-    # Check if the number is prime and display the result
     if is_prime(number):
         print(f"{number} is a prime number.")
     else:
         print(f"{number} is not a prime number.")
 
-# Call the main function to execute the program
 if __name__== "__main__":
     main()
